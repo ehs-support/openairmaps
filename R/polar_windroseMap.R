32b7ce55ec9c44073e788f9f568bfdf410dda249
Kristina Riemer <kristina.riemer@ehs-support.com>
2026-02-18 14:48:06 -0500

    Update legend title

--- R/polar_windroseMap.R
+++ R/polar_windroseMap.R
@@ -287,7 +287,7 @@ windroseMap <- function(
           static = static,
           legend.title.autotext = legend.title.autotext,
           legend.title = legend.title,
-          str = "Wind Speed"
+          str = "Magnitude (mm/s)"
         )
 
       map <-
